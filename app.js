23bb7e139d39473a1a92e91e166e07fb5d49e93f
44RY4N <aaryan23b@iiitg.ac.in>
2026-01-01 16:10:31 +0530

    added environment variables in config.env

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const userRouter = require(`${__dirname}/routes/userRouter.js`);
 // MIDDLEWARE SECTION
 app.use(express.json()); //middleware to parse json data
 
-app.use((req, res, next) => {
-  console.log('Here a middleware ran 👩‍💻🦠');
-  next();
-});
+if (process.env.NODE_ENV === 'developement') {
+  app.use((req, res, next) => {
+    console.log('Here a middleware ran 👩‍💻🦠');
+    next();
+  });
+}
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: `${__dirname}/config.env` });
+
+console.log(process.env);
+
 const app = require(`${__dirname}/app.js`);
-const PORT = 8000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
 });
