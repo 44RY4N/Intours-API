2e169d671265644572a592cdd3816e24c5acb115
44RY4N <aaryan23b@iiitg.ac.in>
2026-01-16 15:28:41 +0530

    updates

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,56 +1,34 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema(
-  {
-    serial: { type: Number },
-    zone: { type: String, required: [true, 'Zone is required'] },
-    state: { type: String, required: [true, 'State is required'] },
-    city: { type: String, required: [true, 'City is required'] },
-    name: {
-      type: String,
-      required: [true, 'Name is required'],
-      unique: [true, 'Name Must be Unique'],
-      trim: true,
-    },
-    type: { type: String },
-    establishmentYear: { type: Number, alias: 'Establishment Year' },
-    timeNeededToVisitInHrs: {
-      type: Number,
-      alias: 'time needed to visit in hrs',
-    },
-    googleReviewRating: { type: Number, alias: 'Google Review Rating' },
-    entranceFeeInInr: { type: Number, alias: 'Entrance Fee in INR' },
-    airportWith50kmRadius: { type: String, alias: 'Airport with 50km Radius' },
-    weeklyOff: { type: String, alias: 'Weekly Off' },
-    significance: { type: String },
-    dslrAllowed: { type: String, alias: 'DSLR Allowed' },
-    numberOfGoogleReviewInLakhs: {
-      type: Number,
-      alias: 'Number of google review in lakhs',
-    },
-    bestTimeToVisit: { type: String, alias: 'Best Time to visit' },
+const tourSchema = new mongoose.Schema({
+  serial: { type: Number },
+  zone: { type: String, required: [true, 'Zone is required'] },
+  state: { type: String, required: [true, 'State is required'] },
+  city: { type: String, required: [true, 'City is required'] },
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    unique: [true, 'Name Must be Unique'],
+    trim: true,
   },
-  // Required for older keys with spacing
-  {
-    toJSON: {
-      virtuals: true,
-      transform(doc, ret) {
-        delete ret.establishmentYear;
-        delete ret.timeNeededToVisitInHrs;
-        delete ret.entranceFeeInInr;
-        delete ret.airportWith50kmRadius;
-        delete ret.weeklyOff;
-        delete ret.dSLRAllowed;
-        delete ret.numberOfGoogleReviewInLakhs;
-        delete ret.bestTimeToVisit;
-        delete ret.__v;
-        delete ret.id;
-        return ret;
-      },
-    },
-    toObject: { virtuals: true },
+  type: { type: String },
+  establishmentYear: { type: Number, alias: 'Establishment Year' },
+  timeNeededToVisitInHrs: {
+    type: Number,
+    alias: 'time needed to visit in hrs',
   },
-);
+  googleReviewRating: { type: Number, alias: 'Google Review Rating' },
+  entranceFeeInInr: { type: Number, alias: 'Entrance Fee in INR' },
+  airportWith50kmRadius: { type: String, alias: 'Airport with 50km Radius' },
+  weeklyOff: { type: String, alias: 'Weekly Off' },
+  significance: { type: String },
+  dslrAllowed: { type: String, alias: 'DSLR Allowed' },
+  numberOfGoogleReviewInLakhs: {
+    type: Number,
+    alias: 'Number of google review in lakhs',
+  },
+  bestTimeToVisit: { type: String, alias: 'Best Time to visit' },
+});
 
 const tours = mongoose.model('tours', tourSchema);
 
